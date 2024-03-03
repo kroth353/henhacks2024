16cd75a4b74da1154b7a77fa31994803e0a2f7e4
Jessica Manchak <jmanchak@udel.edu>
2024-03-02 21:23:40 -0500

    Generating tower

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,62 @@
-import react from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 
 import Horizontal from "./Assets/Horizontal.png";
 import Vertical from "./Assets/Vertical.png";
 import { EquationGenerator } from "./form-components/EquationGenerator";
 
-
+interface ImageStyle {
+    position: 'absolute';
+    id: number,
+    top: string;
+    left: string;
+}
 function App(): JSX.Element {
+    const generateImages = (type: string, startTop: number, startLeft: number, numImages: number, topIncrement: number, leftIncrement: number) => {
+        const images = [];
+        let idNum = 0;
+        //let currentId = idNum;
+
+        for (let i = 0; i < numImages; i++) {
+            const style: ImageStyle = {
+                position: 'absolute',
+                id: i,
+                top: `${startTop + (i * topIncrement)}px`,
+                left: `${startLeft + (i * leftIncrement)}px`
+            };
+
+            images.push(
+                <img
+                    key={`${type}-${i}`}
+                    id={''+idNum}
+                    src={type === 'horizontal' ? Horizontal : Vertical}
+                    style={style}
+                    alt={`${type} ${i}`}
+                    className={type === 'horizontal' ? 'img' : 'images'}
+                />
+            );
+
+            console.log(images[i]);
+
+            idNum++;
+        }
+        
+
+        return images;
+    };
     return (
         <div className="App">
             <header className="App-header">
                 Jenga
             </header>
-            <div>
-            <body>
-                        <img src={Horizontal} style={{ position: 'absolute', top: '300px', left: '80px' }} alt="Horizontal" className="img"></img>
-                        <img src={Horizontal} style={{ position: 'absolute', top: '320px', left: '120px' }} alt="Horizontal" className="img"></img>
-                        <img src={Horizontal} style={{ position: 'absolute', top: '340px', left: '160px' }} alt="Horizontal" className="img"></img>
-
-                        <img src={Vertical} style={{ position: 'absolute', top: '250px', left: '120px' }} alt="Horizontal" className="images"></img>
-                        <img src={Vertical} style={{ position: 'absolute', top: '270px', left: '80px' }} alt="Horizontal" className="images"></img>
-                        <img src={Vertical} style={{ position: 'absolute', top: '290px', left: '40px' }} alt="Horizontal" className="images"></img>
-
-                        <img src={Horizontal} style={{ position: 'absolute', top: '200px', left: '80px' }} alt="Horizontal" className="images"></img>
-                        <img src={Horizontal} style={{ position: 'absolute', top: '220px', left: '120px' }} alt="Horizontal" className="images"></img>
-                        <img src={Horizontal} style={{ position: 'absolute', top: '240px', left: '160px' }} alt="Horizontal" className="images"></img>
-                </body>
-            </div>
+        <body>
+        {generateImages('horizontal', 300, 80, 3, 20, 40)}
+        {generateImages('vertical', 250, 120, 3, 20, -40)}
+        {generateImages('horizontal', 200, 80, 3, 20, 40)}
+        {generateImages('vertical', 150, 120, 3, 20, -40)}
+        {generateImages('horizontal', 100, 80, 3, 20, 40)}
+        {generateImages('vertical', 50, 120, 3, 20, -40)}
+            </body>
             <hr></hr>
             <EquationGenerator></EquationGenerator>
        </div>
