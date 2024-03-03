eba3e257a9e42885431455f430cdf147cee5385c
Kayla Roth <121525795+kroth353@users.noreply.github.com>
2024-03-03 09:53:31 -0500

    Revert "Added grade selection drop down"

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-export function GradeLevelSelection({
-    options
+export function MultipleChoiceQuestion({
+    options,
+    expectedAnswer
 }: {
     options: string[];
+    expectedAnswer: string;
 }): JSX.Element {
     const [choice, setChoice] = useState<string>("");
 
@@ -15,15 +17,16 @@ export function GradeLevelSelection({
     //This is the View
     return (
         <div>
+            <h3>Multiple Choice Question</h3>
             <Form.Group>
-                <Form.Label>Choose your Grade Level</Form.Label>
+                <Form.Label>Choose your answer:</Form.Label>
                 <Form.Select value={choice} onChange={updateChoice}>
                     {options.map((myChoice: string) => (
                         <option key={myChoice}>{myChoice}</option>
                     ))}
                 </Form.Select>
             </Form.Group>
-            <div>Selected: {choice}</div>
+            <div>Your answer is {choice == expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
