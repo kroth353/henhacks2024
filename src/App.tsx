7440484635c93840ae79ee4a2c381212e2431481
Kayla Roth <kaylaroth353@gmail.com>
2024-03-02 20:16:01 -0500

    Updated blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import react from 'react';
 import "./App.css";
 
 import Horizontal from "./Assets/Horizontal.png";
@@ -12,19 +12,21 @@ function App(): JSX.Element {
             <header className="App-header">
                 Jenga
             </header>
-        <body>
-                <img src={Horizontal} style={{ position: 'absolute', top: '25px', left: '60px' }} alt="Horizontal" className="img"></img>
-                <img src={Horizontal} style={{ position: 'absolute', top: '35px', left: '80px' }} alt="Horizontal" className="img"></img>
-                <img src={Horizontal} style={{ position: 'absolute', top: '45px', left: '100px' }} alt="Horizontal" className="img"></img>
+            <div>
+            <body>
+                        <img src={Horizontal} style={{ position: 'absolute', top: '300px', left: '80px' }} alt="Horizontal" className="img"></img>
+                        <img src={Horizontal} style={{ position: 'absolute', top: '320px', left: '120px' }} alt="Horizontal" className="img"></img>
+                        <img src={Horizontal} style={{ position: 'absolute', top: '340px', left: '160px' }} alt="Horizontal" className="img"></img>
 
-                <img src={Vertical} style={{ position: 'absolute', top: '0px', left: '80px' }} alt="Horizontal" className="images"></img>
-                <img src={Vertical} style={{ position: 'absolute', top: '10px', left: '60px' }} alt="Horizontal" className="images"></img>
-                <img src={Vertical} style={{ position: 'absolute', top: '20px', left: '40px' }} alt="Horizontal" className="images"></img>
+                        <img src={Vertical} style={{ position: 'absolute', top: '250px', left: '120px' }} alt="Horizontal" className="images"></img>
+                        <img src={Vertical} style={{ position: 'absolute', top: '270px', left: '80px' }} alt="Horizontal" className="images"></img>
+                        <img src={Vertical} style={{ position: 'absolute', top: '290px', left: '40px' }} alt="Horizontal" className="images"></img>
 
-                <img src={Horizontal} style={{ position: 'absolute', top: '-25px', left: '60px' }} alt="Horizontal" className="images"></img>
-                <img src={Horizontal} style={{ position: 'absolute', top: '-15px', left: '80px' }} alt="Horizontal" className="images"></img>
-                <img src={Horizontal} style={{ position: 'absolute', top: '-5px', left: '100px' }} alt="Horizontal" className="images"></img>
-            </body>
+                        <img src={Horizontal} style={{ position: 'absolute', top: '200px', left: '80px' }} alt="Horizontal" className="images"></img>
+                        <img src={Horizontal} style={{ position: 'absolute', top: '220px', left: '120px' }} alt="Horizontal" className="images"></img>
+                        <img src={Horizontal} style={{ position: 'absolute', top: '240px', left: '160px' }} alt="Horizontal" className="images"></img>
+                </body>
+            </div>
             <hr></hr>
             <EquationGenerator></EquationGenerator>
        </div>
